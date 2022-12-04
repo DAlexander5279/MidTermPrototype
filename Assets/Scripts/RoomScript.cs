772550ef02b94e02fa6b1a13f48785ea72b8bd3c
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2022-12-04 18:24:08 -0500

    Added seperate script for door functionality

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 public class RoomScript : MonoBehaviour
 {
     [SerializeField] GameObject roomPref;
-    [SerializeField] GameObject door;
-    [SerializeField] GameObject OpenPos;
-    [SerializeField] GameObject ClosePos;
     [SerializeField] GameObject spawnPos;
-    [SerializeField] float doorSpeedOpen;
-    [SerializeField] float doorSpeedClose;
-    [SerializeField] int enemyTemp;
     [SerializeField] bool roomSpawned;
-    [SerializeField] bool doorOpened;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,43 +17,24 @@ public class RoomScript : MonoBehaviour
     void Update()
     {
 
-        if(enemyTemp <= 0)
+      if(!roomSpawned)
         {
-            OpenDoor();
-
-            if(!roomSpawned)
-            {
-                SpawnRoom();
-                roomSpawned = true;
-            }
+            SpawnRoom();
+            roomSpawned = true;
         }
-
+      
 
     }
 
-    void OpenDoor()
-    {
-        door.transform.position = Vector3.Lerp(door.transform.position, OpenPos.transform.position, Time.deltaTime * doorSpeedOpen);
-    }
 
-    void CloseDoor()
-    {
-        door.transform.position = Vector3.Lerp(door.transform.position, ClosePos.transform.position, Time.deltaTime * doorSpeedClose);
-    }
     void SpawnRoom()
     {
         Instantiate(roomPref, spawnPos.transform.position, transform.rotation);
     }
 
-    public void OnTriggerEnter(Collider other)
-    {
-
-    }
 
     public void OnTriggerExit(Collider other)
     {
-        CloseDoor();
-        new WaitForSeconds(2);
         Destroy(gameObject);
     }
 
