b51dcf73c2fe115584fe2322a451ffa200b26d62
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2022-12-04 19:40:40 -0500

    Fix

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class RoomScript : MonoBehaviour
 {
     [SerializeField] GameObject roomPref;
-    [SerializeField] GameObject door;
-    [SerializeField] GameObject OpenPos;
-    [SerializeField] GameObject ClosePos;
     [SerializeField] GameObject spawnPos;
-    [SerializeField] float doorSpeedOpen;
-    [SerializeField] float doorSpeedClose;
-    [SerializeField] int enemyTemp;
     [SerializeField] bool roomSpawned;
-    [SerializeField] bool doorOpened;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,40 +18,18 @@ public class RoomScript : MonoBehaviour
     void Update()
     {
 
-<<<<<<< HEAD
+
         if (!roomSpawned && gameManager.instance.enemyCount <= 0)
         {
             SpawnRoom();
-
-=======
-        // if (gameManager.instance.enemyCount <= 0)
-        if (enemyTemp <= 0)
-            {
-                OpenDoor();
-
-            if(!roomSpawned)
-            {
-                SpawnRoom();
-                roomSpawned = true;
-            }
->>>>>>> ce25464b00ba877dba95fbf22ca9004102255dca
         }
 
 
     }
 
-    void OpenDoor()
-    {
-        door.transform.position = Vector3.Lerp(door.transform.position, OpenPos.transform.position, Time.deltaTime * doorSpeedOpen);
-    }
 
-    void CloseDoor()
-    {
-        door.transform.position = Vector3.Lerp(door.transform.position, ClosePos.transform.position, Time.deltaTime * doorSpeedClose);
-    }
     void SpawnRoom()
     {
-        gameManager.instance.enemyCount++;
         Instantiate(roomPref, spawnPos.transform.position, transform.rotation); 
         roomSpawned = true;
     }
@@ -69,8 +41,6 @@ public class RoomScript : MonoBehaviour
 
     public void OnTriggerExit(Collider other)
     {
-        CloseDoor();
-        new WaitForSeconds(2);
         Destroy(gameObject);
     }
 
