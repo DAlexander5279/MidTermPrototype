e301b7a301e9857d0bbdd7cb598a8b016d25fcc2
DAlexander5279 <104796691+DAlexander5279@users.noreply.github.com>
2023-01-16 14:49:49 -0500

    WaveManager

---
+++ Assets/Scripts/waveManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveManager : MonoBehaviour
+{
+
+
+    [SerializeField] GameObject[] positions;
+    [SerializeField] GameObject[] enemies;
+
+
+
+    [SerializeField] int WaveEnemyCount;
+    [SerializeField] int EnemiesSpawned;
+    public int AliveEnemyCount;
+    [SerializeField] int wave;
+    public bool isWaveOver;
+    [SerializeField] float timeBetweenSpawns;
+    [SerializeField] int waveBreakTimer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        wave = 0;
+        isWaveOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //checks if wave is over, and if enemies need to be spawned
+        if(!isWaveOver && EnemiesSpawned < WaveEnemyCount)
+        {
+           StartCoroutine(spawnEnemies());
+        }
+
+        //checks if any enemies are alive
+        if(AliveEnemyCount == 0)
+        {
+            isWaveOver = true;
+            StartCoroutine(waveBreak());
+        }
+    }
+
+
+    IEnumerator spawnEnemies()
+    {
+        //spawns random enemy in a random location
+        Instantiate(enemies[Random.Range(0, enemies.Length)], positions[Random.Range(0, positions.Length)].transform);
+        
+        //increments the number of enemies that have been spawned
+        EnemiesSpawned++;
+
+        //waits x amount of time in between each spawn
+        yield return new WaitForSeconds(timeBetweenSpawns);
+    }
+
+    IEnumerator waveBreak()
+    {
+        //THIS IS WHEN THE SHOP CAN BE ACCESSED
+
+
+        //waits x amount of time, then resets variables for new wave
+        yield return new WaitForSeconds(waveBreakTimer);
+        EnemiesSpawned = 0;
+        //increments enemy count for the next wave
+        WaveEnemyCount += 3;
+        isWaveOver = false;
+      
+    }
+
+
+}
+
