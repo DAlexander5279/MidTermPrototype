964e0fddfe3062b62b699e66ff6dcccd084a2223
ilrathCXV <ilrathtdm@gmail.com>
2023-01-18 13:49:08 -0800

    Wave Manager completed (boss waves + higher difficulty waves)
    Also duplicated Dalton's scene as my new scene due to Arena not transferring properly when placed in my scene

--- Assets/Scripts/waveManager.cs
+++ Assets/Scripts/waveManager.cs
@@ -8,22 +8,31 @@ public class waveManager : MonoBehaviour
 
     [SerializeField] GameObject[] positions;
     [SerializeField] GameObject[] enemies;
+    [SerializeField] GameObject[] bossList;
 
 
 
     [SerializeField] int WaveEnemyCount;
+    [SerializeField] int WaveBossEnemyCount;
     [SerializeField] int EnemiesSpawned;
     public int AliveEnemyCount;
     [SerializeField] int wave;
     public bool isWaveOver;
     [SerializeField] float timeBetweenSpawns;
     [SerializeField] float waveBreakTimer;
+    public bool isSpawningEnemies;
+    public bool isBossRound;
+    public bool isHigherDiff;
 
     // Start is called before the first frame update
     private void Start()
     {
         wave = 0;
-        isWaveOver = false;
+        isWaveOver = false;          // set to true to allow some time before the first wave starts
+        isSpawningEnemies = true;  // using this will allow the game to make use of the spawner delay
+        isBossRound = false;
+        isHigherDiff = false;
+        StartCoroutine(waveBreak());
 
     }
 
@@ -33,19 +42,72 @@ public class waveManager : MonoBehaviour
 
         AliveEnemyCount = gameManager.instance.enemyCount;
         //checks if wave is over, and if enemies need to be spawned
-        if (!isWaveOver)
+        if (!isWaveOver && (wave > 0))
         {
-            if (EnemiesSpawned < WaveEnemyCount)
-                StartCoroutine(spawnEnemies());
+            if (!isSpawningEnemies)
+            {
+                if ((wave > 15) && (wave % 5 != 0) && (EnemiesSpawned < (WaveEnemyCount + WaveBossEnemyCount)))
+                {
+                    isHigherDiff = true;
+                    isBossRound = false;
+                    isSpawningEnemies = true;
+                    StartCoroutine(spawnEnemies());
+                    StartCoroutine(spawnBossEnemies());
+                }
+                else if ((wave % 5 == 0) && (EnemiesSpawned < WaveBossEnemyCount))
+                {
+                    isHigherDiff = false;
+                    isBossRound = true;
+                    isSpawningEnemies = true;
+                    StartCoroutine(spawnBossEnemies());
+                }
+                else if ((wave % 5 != 0) && EnemiesSpawned < WaveEnemyCount)
+                {
+                    isHigherDiff = false;
+                    isBossRound = false;
+                    isSpawningEnemies = true;
+                    StartCoroutine(spawnEnemies());
+                }
+
+            }
+
         }
 
         //checks if any enemies are alive
         //if(AliveEnemyCount == 0)
         if (gameManager.instance.enemyCount == 0 && !isWaveOver)
         {
-            isWaveOver = true;
+            if (isHigherDiff && !isBossRound)
+            {
+                if (EnemiesSpawned == (WaveEnemyCount + WaveBossEnemyCount))
+                {
+                    isWaveOver = true;
 
-            StartCoroutine(waveBreak());
+                    StartCoroutine(waveBreak());
+
+                }
+            }
+            else if (isBossRound)
+            {
+                if (EnemiesSpawned ==  WaveBossEnemyCount)
+                {
+                    isWaveOver = true;
+
+                    StartCoroutine(waveBreak());
+
+                }
+            }
+            else
+            {
+                if (EnemiesSpawned == WaveEnemyCount)
+                {
+                    isWaveOver = true;
+
+                    StartCoroutine(waveBreak());
+
+                }
+            }
+
         }
     }
 
@@ -64,6 +126,7 @@ public class waveManager : MonoBehaviour
 
         //waits x amount of time in between each spawn
         yield return new WaitForSeconds(timeBetweenSpawns);
+        isSpawningEnemies = false;
     }
 
     IEnumerator waveBreak()
@@ -71,15 +134,36 @@ public class waveManager : MonoBehaviour
         //THIS IS WHEN THE SHOP CAN BE ACCESSED
         EnemiesSpawned = 0;
         //increments enemy o
-        WaveEnemyCount += Mathf.FloorToInt(gameManager.instance.scalingFunction(3 + Mathf.FloorToInt(wave * 0.2f)));
+        if (wave > 0)
+        {
+            WaveEnemyCount += Mathf.FloorToInt(gameManager.instance.scalingFunction(3 + Mathf.FloorToInt(wave * 0.2f)));
+            WaveBossEnemyCount += Mathf.FloorToInt(gameManager.instance.scalingFunction(Mathf.FloorToInt(wave * 0.2f)));
+        }
 
         //waits x amount of time, then resets variables for new wave
         yield return new WaitForSeconds(waveBreakTimer);
-
+        wave++;
+        isSpawningEnemies = false;
         isWaveOver = false;
 
     }
+    IEnumerator spawnBossEnemies()
+    {
 
+        int indexEnemy = Random.Range(0, bossList.Length);
+        int indexPos = Random.Range(0, positions.Length);
+        //spawns random enemy in a random location
+
+        Instantiate(bossList[indexEnemy], positions[indexPos].transform.position, Quaternion.identity);
+
+        //increments the number of enemies that have been spawned
+        EnemiesSpawned++;
+
+        //waits x amount of time in between each spawn
+        yield return new WaitForSeconds(timeBetweenSpawns * 3.0f);
+
+        isSpawningEnemies = false;
+    }
 
 }
 
